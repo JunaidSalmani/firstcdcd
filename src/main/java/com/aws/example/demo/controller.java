496c9f2721e8ab180c16913231b0aa2df496baf4
JunaidSalmani <juned.salmani835@gmail.com>
2025-05-25 02:11:51 +0530

    buildspec added;

--- src/main/java/com/aws/example/demo/controller.java
+++ src/main/java/com/aws/example/demo/controller.java
@@ -1,8 +1,12 @@
 package com.aws.example.demo;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class controller {
+	@GetMapping("/data")
+	public String getDate() {
+return "first date";		
+	}
 
 }
